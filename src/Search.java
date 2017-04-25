57fa0fb893efdb8a7e2360c9ca356d836a658868
Seandunn92 <seandunn92@gmail.com>
2017-04-25 16:10:16 -0400

    updated Search

--- src/Search.java
+++ src/Search.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by PAS8 on 4/25/2017.
@@ -6,7 +7,9 @@ import java.util.ArrayList;
 public class Search {
 
     //Method to search book by author
-    static void searchBookByAuthor(ArrayList <Book> bookList, String author) {
+    static void searchBookByAuthor(ArrayList <Book> bookList, Scanner scan) {
+        System.out.println("Enter the author's name");
+        String author = scan.nextLine();
         for (Book temp : bookList) {
             if (temp.getAuthor ().equals ( author )) {
                 System.out.println ( temp );
@@ -15,7 +18,9 @@ public class Search {
     }
 
     //Method to search book by title
-    static void searchBookByTitle(ArrayList <Book> bookList, String title) {
+    static void searchBookByTitle(ArrayList <Book> bookList, Scanner scan) {
+        System.out.println("Enter in the book's title");
+        String title = scan.nextLine();
         for (Book temp : bookList) {
             if (temp.getTitle ().equals ( title )) {
                 System.out.println ( temp );
